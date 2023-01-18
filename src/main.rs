406d096cf52b878fc395afa44ada35918b62eabc
JM <j.maxwell.alan@gmail.com>
2023-01-18 18:14:13 -0500

    uncomment solutions

--- src/main.rs
+++ src/main.rs
@@ -30,40 +30,40 @@ pub fn js_run(filename: &str, args: &[&str]) -> String {
 }
 
 fn main() {
-  // println!("1. Multiples of 3 or 5: {}", mult_three_five());
-  // println!("2. Even Fibonacci numbers: {}", even_fibonacci_numbers());
-  // println!("3. Largest prime factor: {}", largest_prime_factor());
-  // println!(
-  //   "4. Largest palindrome product: {}",
-  //   largest_palindrome_product()
-  // );
-  // println!("5. Smallest multiple: {}", smallest_multiple());
-  // println!("6. Sum square difference: {}", sum_square_difference());
-  // println!("7. 10001st prime: {}", ten_thousand_first_prime());
-  // println!(
-  //   "8. Largest product in a series: {}",
-  //   largest_product_in_series()
-  // );
-  // println!(
-  //   "9. Special pythagorean triplet: {}",
-  //   special_pythagorean_triplet()
-  // );
-  // println!("10. Summation of primes: {}", summation_of_primes());
-  // println!("11. Largest product in grid: {}", largest_product_in_grid());
-  // println!(
-  //   "12. Highly divisible triangular number: {}",
-  //   highly_divisible_triangular_number()
-  // );
-  // println!("13. Large sum: {}", js_run("thirteen", &[]));
-  // println!(
-  //   "14. Longest Collatz sequence: {}",
-  //   longest_collatz_sequence()
-  // );
-  // println!("15. Lattice paths: {}", lattice_paths());
-  // println!("16. Power digit sum: {}", js_run("sixteen", &[]));
-  // println!("17. Number letter counts: {}", number_letter_counts());
-  // println!("18. Maximum path sum I: {}", maximum_path_sum_i());
-  // println!("19. Counting Sundays: {}", counting_sundays());
+  println!("1. Multiples of 3 or 5: {}", mult_three_five());
+  println!("2. Even Fibonacci numbers: {}", even_fibonacci_numbers());
+  println!("3. Largest prime factor: {}", largest_prime_factor());
+  println!(
+    "4. Largest palindrome product: {}",
+    largest_palindrome_product()
+  );
+  println!("5. Smallest multiple: {}", smallest_multiple());
+  println!("6. Sum square difference: {}", sum_square_difference());
+  println!("7. 10001st prime: {}", ten_thousand_first_prime());
+  println!(
+    "8. Largest product in a series: {}",
+    largest_product_in_series()
+  );
+  println!(
+    "9. Special pythagorean triplet: {}",
+    special_pythagorean_triplet()
+  );
+  println!("10. Summation of primes: {}", summation_of_primes());
+  println!("11. Largest product in grid: {}", largest_product_in_grid());
+  println!(
+    "12. Highly divisible triangular number: {}",
+    highly_divisible_triangular_number()
+  );
+  println!("13. Large sum: {}", js_run("thirteen", &[]));
+  println!(
+    "14. Longest Collatz sequence: {}",
+    longest_collatz_sequence()
+  );
+  println!("15. Lattice paths: {}", lattice_paths());
+  println!("16. Power digit sum: {}", js_run("sixteen", &[]));
+  println!("17. Number letter counts: {}", number_letter_counts());
+  println!("18. Maximum path sum I: {}", maximum_path_sum_i());
+  println!("19. Counting Sundays: {}", counting_sundays());
   println!("20. Factorial digit sum: {}", js_run("twenty", &[]));
-  // println!("67. Maximum path sum II: {}", maximum_path_sum_ii());
+  println!("67. Maximum path sum II: {}", maximum_path_sum_ii());
 }
